4390772a7ce228da111750b87e4a49aaceb67596
Giang <vuhoanggiang203@gmail.com>
2024-06-08 14:23:25 +0700

    first

---
+++ functions/index.js
@@ -0,0 +1,90 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *  const URL_TELEGRAM_WEBHOOK = 'https://api.telegram.org/bot{TOKEN}/';
+//  const URL_GET_OHLC="https://www.okx.com/api/v5/market/history-mark-price-candles?instId=BTC-USDT&bar=1m&limit=100&after=";
+//  const URL_GETCOIN="https://www.okx.com/api/v5/rubik/stat/trading-data/support-coin";
+ */
+require("dotenv").config();
+const axios = require("axios");
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+require("firebase-functions/v2/firestore");
+const {initializeApp} = require("firebase-admin/app");
+require("firebase-admin/firestore");
+const TelegramBot = require("node-telegram-bot-api");
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token, {polling: true});
+initializeApp();
+require("dotenv").config();
+const convertArray = (arr) => arr.map((innerArray) =>
+  innerArray.map((item) => {
+    const number = parseFloat(item);
+    return isNaN(number) ? item : number;
+  }),
+);
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+const last3days = Date.now() - 259200000;
+exports.coins = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  const url1 = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://www.okx.com/api/v5/rubik/stat/trading-data/support-coin",
+  };
+  axios.request(url1)
+      .then((res) => {
+        console.log(JSON.stringify(res.data.data));
+        const result = JSON.stringify(res.data.data);
+        response.send(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+});
+exports.price = onRequest((request, response) => {
+  logger.info("a", {structuredData: true});
+  const url2 = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://www.okx.com/api/v5/market/history-mark-price-candles?instId=BTC-USDT&bar=1m&limit=100&after="+last3days,
+  };
+  axios.request(url2)
+      .then((res) => {
+        console.log(JSON.stringify(res.data.data));
+        const result = JSON.stringify(res.data.data);
+        console.log(typeof(result));
+        const dataArray = JSON.parse(result);
+        const finalResult = convertArray(dataArray);
+        console.log(typeof(finalResult));
+        response.send(finalResult);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+});
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Received your message");
+});
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const welcomeMessage = `Hello, ${msg.from.first_name}! Welcome to the bot. `;
+  bot.sendMessage(chatId, welcomeMessage);
+});
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  const help = " \n/start - Start the bot\n/help-Show help message\n";
+  const helpMessage = "Here are the available commands:" + help;
+  bot.sendMessage(chatId, helpMessage);
+});
+bot.onText(/\/BTC/, (msg) => {
+  const chatId = msg.chat.id;
+  const helpMessage = "THIS IS PRICE OF BTC LAST 3 DAYS";
+  bot.sendMessage(chatId, helpMessage);
+});
